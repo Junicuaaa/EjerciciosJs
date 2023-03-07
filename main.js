fcff394c209369ff717491125440bfb928638d7a
JuniorSantiagoCampus <juniorcarvajalino1213@gmail.com>
2023-03-07 18:00:52 -0500

    feat: metodo 8

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 let cadena = "me gusta mucho campus"
-console.log(cadena.repeat(2))
-/** El método repeat() construye y devuelve una nueva cadena que contiene el número especificado de
-copias de la cadena en la cual fue llamada, concatenados.
+console.log(cadena.toLowerCase())
+/** Transforma el texto a minuscula
  */
