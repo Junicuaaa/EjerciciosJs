ad5c4f24a302b9286dd58fd12ec5f427e3576411
JuniorSantiagoCampus <juniorcarvajalino1213@gmail.com>
2023-03-07 17:21:33 -0500

    feat: metodo 4

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 let cadena = "me gusta mucho campus"
-console.log(cadena.startsWith("c"))
-// Nos dice si la cadena comienza con (x) caracter//
+console.log(cadena.endsWith("c"))
+// Nos dice si la cadena finaliza con (x) caracter//
