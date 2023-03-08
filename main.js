178deac906ad98bb7f17e5a6d94dd91daebb16a0
JuniorSantiagoCampus <juniorcarvajalino1213@gmail.com>
2023-03-07 19:58:02 -0500

    feat: metodo 17

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-console.log("Campus Land".indexOf("u"))
-/** devuelve el índice, dentro del objeto String que realiza la llamada, 
- * de la primera ocurrencia del valor especificado, comenzando la búsqueda desde indiceDesde; o -1 si no se encuentra dicho valor.
+console.log("Campus Land".lastIndexOf("u"))
+/**  devuelve la posicion (indice) en la que se encuentra el valorBusqueda, 
+ * dentro del objeto String que realiza la llamada, de la última ocurrencia del valor especificada.
  */
