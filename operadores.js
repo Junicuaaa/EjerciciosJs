2a5cc3f5c7030e9d7cb96ad19b1fadefada9a412
JuniorSantiagoCampus <juniorcarvajalino1213@gmail.com>
2023-03-09 07:08:07 -0500

    feat: :zap: Operadores de comparacion

--- operadores.js
+++ operadores.js
@@ -1,3 +1,12 @@
-/* Operador de as ignacion */  
-let x = 12
-console.log(`el valor asignado a x es ${x}`)
+/* Operadores de comparación*/  
+let x = 20
+let y = "20"
+let b = 20
+let a = 40
+console.log(`Operador de igualdad == "No Estricto" ${x == y}`)
+console.log(`Operador de desigualdad != "No Estricto" ${x == a}`)
+console.log(`Operador de igualdad === "Estricto" ${x == b}`)
+console.log(`Operador de mayor que > ${a > x}`)
+console.log(`Operador de menor que < ${x < a}`)
+console.log(`Operador de mayor o igual que >= ${x >= b}`)
+console.log(`Operador de menor o igual que <= ${x <= a}`)
