a50830aec38c5ee9ca0f33aa19889ab376d7bc6b
JuniorSantiagoCampus <juniorcarvajalino1213@gmail.com>
2023-03-09 07:48:52 -0500

    feat: Operadores aritmeticos

--- operadores.js
+++ operadores.js
@@ -1,12 +1,6 @@
-/* Operadores de comparación*/  
-let x = 20
-let y = "20"
-let b = 20
-let a = 40
-console.log(`Operador de igualdad == "No Estricto" ${x == y}`)
-console.log(`Operador de desigualdad != "No Estricto" ${x == a}`)
-console.log(`Operador de igualdad === "Estricto" ${x == b}`)
-console.log(`Operador de mayor que > ${a > x}`)
-console.log(`Operador de menor que < ${x < a}`)
-console.log(`Operador de mayor o igual que >= ${x >= b}`)
-console.log(`Operador de menor o igual que <= ${x <= a}`)
+/* Operadores aritméticos
+*/
+let x = 5%5;
+console.log(`Operador de Residuo % ${x}`)
+console.log(`Operador de incremento ++`)
+console.log(`Operador de decremento --`)
