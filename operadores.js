6b1eca5815c5172008cad452cb3b6400ccf2a965
JuniorSantiagoCampus <juniorcarvajalino1213@gmail.com>
2023-03-09 08:37:17 -0500

    feat:

--- operadores.js
+++ operadores.js
@@ -1,6 +1,8 @@
-/* Operadores aritméticos
+/* Operadores Logicos
 */
-let x = 5%5;
-console.log(`Operador de Residuo % ${x}`)
-console.log(`Operador de incremento ++`)
-console.log(`Operador de decremento --`)
+let x = 10;
+let y = 5;
+let a = 15;
+
+console.log(`Operador || (or) ${a < x || a > y}`);
+console.log(`Operador && (and) ${a < x && a > y}`);
